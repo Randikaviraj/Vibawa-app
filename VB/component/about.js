805f30e309c2cfcbd8d4c2b3c46a849a13ca4d35
Randikaviraj <randikavirajmax@gmail.com>
2020-06-27 05:54:55 +0530

    final with updated about

--- VB/component/about.js
+++ VB/component/about.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import  {View,Text} from 'react-native';
+import  {View,Image,ScrollView,StyleSheet} from 'react-native';
 
 
 export default function About(){
     return(
-        <View>
-                <Text>about</Text>
-                
+        <View style={{flex:1}}>
+           
+                <View style={{...StyleSheet.absoluteFill,}} >
+                    <Image
+                        source={require('../assets/images/about.jpg')}
+                            style={{height:null,width:null,flex:1}}/>
+                </View>
+           
         </View>
     )
 }
